827b7d3c31fec60c563e4fcb2d68ac0a29394fc5
hawkyjl <hawkyjl@gmail.com>
2017-11-10 12:49:51 +0800

    Initial commit

---
+++ Assets/Scripts/Scene/Loading/adaaaaaa.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+
+public class adaaaaaa : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        //loadScene();
+        loadObject();
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+    void loadObject()
+    {
+        var myLoadedAssetBundle = AssetBundle.LoadFromFile(Application.dataPath + "/AssetBundles/startgame.something");
+        if (myLoadedAssetBundle == null)
+        {
+            Debug.Log("Failed to load AssetBundle!");
+            return;
+        }
+        var prefab = myLoadedAssetBundle.LoadAsset<GameObject>("Enemy1");
+        Instantiate(prefab);
+    }
+    void loadScene()
+    {
+        var myLoadedAssetBundle = AssetBundle.LoadFromFile(Application.dataPath + "/AssetBundles/startgame.scene");
+        if (myLoadedAssetBundle == null)
+        {
+            Debug.Log("Failed to load AssetBundle!");
+            return;
+        }
+        string[] scenePath = myLoadedAssetBundle.GetAllScenePaths();
+        UnityEngine.SceneManagement.SceneManager.LoadScene(scenePath[0]);
+    }
+}
